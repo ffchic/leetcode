fbd72fbee0c023dd201fd9825e30527e6f6dc563
unknown <fuhongrui@lettucegames.com>
2022-12-29 18:53:18 +0800

    括号生成-回溯算法实现

---
+++ 括号生成backtracking/README.md
@@ -0,0 +1,24 @@
+<div class="description__2b0C"><div data-show-mask="false" class="css-xfm0cl-Container eugt34i0"><h4 data-cypress="QuestionTitle" class="css-10c1h40-Title eugt34i1"><a href="/problems/generate-parentheses/">22. 括号生成</a></h4></div><div class="css-1rngd9y-ZoomWrapper e13l6k8o9"><div class="content__1Y2H"><div class="notranslate"><p>数字 <code>n</code>&nbsp;代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 <strong>有效的 </strong>括号组合。</p>
+
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p>
+
+<pre><strong>输入：</strong>n = 3
+<strong>输出：</strong>["((()))","(()())","(())()","()(())","()()()"]
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre><strong>输入：</strong>n = 1
+<strong>输出：</strong>["()"]
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li><code>1 &lt;= n &lt;= 8</code></li>
+</ul>
---
+++ 括号生成backtracking/code.py
@@ -0,0 +1,36 @@
+"""
+使用递归方法先加一个左括号，然后依次尝试加一个左括号或加一个右括号，如果右边括号数量大于左边则是无效括号直接返回，知道最后左括号数量和右括号数量和n都相等则代表这是一个有效括号，添加到list中即可
+
+作者：clever-chateletxrp
+链接：https://leetcode.cn/problems/generate-parentheses/solution/di-gui-shi-xian-by-clever-chateletxrp-9ycz/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+"""
+def backtracking(n, result, left, right, str):
+    """
+    :param result: 要返回的list
+    :param left: 左边括号的数量
+    :param right: 右边括号的数量
+    :param str: 括号拼接的字符串
+    :return:
+    """
+    if right > left:
+        return
+    elif right == left == n:
+        result.append(str)
+        return
+    if left < n:
+        backtracking(n, result, left+1, right, str+"(")
+    if right < left:
+        backtracking(n, result, left,right+1,str+")")
+
+
+
+class Solution:
+    def generateParenthesis(self, n):
+        result = []
+        backtracking(n, result, 0, 0, '')
+        return result
+
+s = Solution()
+print(s.generateParenthesis(3))
